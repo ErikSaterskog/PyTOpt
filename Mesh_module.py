d2ccdfca71cd19a70b8cc65210799842df42b313
Daniel Pettersson <dapet@student.chalmers.se>
2021-01-27 13:49:05 +0100

    Adding GMSH and the mesh creater-Class

---
+++ Mesh_module.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 09:51:30 2021
+
+@author: Daniel
+"""
+import numpy as np
+import calfem.mesh as cfm
+
+class Mesh:
+    
+    def __init__(self,g,elsize):
+        self.mesh = cfm.GmshMesh(g)
+        self.mesh.dofs_per_node = 2  # Degrees of freedom per node.
+        self.mesh.el_size_factor = elsize  # Factor that changes element sizes.
+        
+    
+    def quad(self):
+        self.mesh.el_type = 3
+        
+        # mesh creation 
+        coords, edof, dofs, bdofs, elementmarkers = self.mesh.create()
+        
+        return coords, edof, dofs, bdofs
+    
+        
+    def tri(self):
+        self.mesh.el_type = 2
+        
+        coords, edof, dofs, bdofs, elementmarkers = self.mesh.create()
+        return coords, edof, dofs, bdofs
+        
+    
+    
---
+++ Mesh_test.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 09:52:07 2021
+
+@author: Daniel
+"""
+
+
+#Really simple test
+import numpy as np
+import calfem.geometry as cfg
+import Mesh_module
+import calfem.vis as cfv
+
+g = cfg.Geometry()
+
+g.point([0,0])          #0
+g.point([1,0])          #1
+g.point([1,0.25],marker=9)       #2
+g.point([0.25,0.25])    #3
+
+g.point([0.25,1])       #4
+
+g.point([0,1])          #5
+
+
+g.spline([0, 1],marker=0)
+
+g.spline([1, 2],marker=1)
+
+g.spline([2, 3],marker=2)
+
+          
+g.spline([3, 4],marker=3)
+
+g.spline([4, 5],marker=4)
+
+g.spline([5, 0],marker=5)
+
+
+g.surface([0, 1, 2, 3,4,5])
+
+_mesh = Mesh_module.Mesh(g,0.05)
+
+coords, edof, dofs, bdofs = _mesh.tri()
+
+cfv.drawMesh(coords, edof, 2, 3)
+#cfv.figure()
