a2200eea0a76ba306e7aa8b27fc547e38c072546
Erik Säterskog <eric.saterskog@gmail.com>
2021-01-28 17:30:35 +0100

    ...

--- Test_Examples/Cantilever_Beam.py
+++ Test_Examples/Cantilever_Beam.py
@@ -18,21 +18,17 @@ g = cfg.Geometry()
 g.point([0,0])                  #0
 g.point([1,0])                  #1
 g.point([1,0.25],marker=9)      #2
-g.point([0.25,0.25])            #3
-g.point([0.25,1])               #4
-g.point([0,1])                  #5
+g.point([0,0.25])               #3
 
 g.spline([0, 1],marker=0)
 g.spline([1, 2],marker=1)
 g.spline([2, 3],marker=2)
-g.spline([3, 4],marker=3)
-g.spline([4, 5],marker=4)
-g.spline([5, 0],marker=5)
+g.spline([3, 0],marker=3)
 
-g.surface([0, 1, 2, 3,4,5])
+g.surface([0, 1, 2, 3])
 
 force = [-1e6,9,2] #First magnutude, second marker,third direction
-bmarker = 4
+bmarker = 3
 
 Main._Main(g,el_type,force,bmarker)
 
