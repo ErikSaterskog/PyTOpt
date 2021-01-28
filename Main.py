889331ce5d6d1f209a04a01b61b80a42c32e9ba1
Daniel Pettersson <dapet@student.chalmers.se>
2021-01-28 09:50:05 +0100

    ....

--- Main.py
+++ Main.py
@@ -7,10 +7,13 @@ import calfem.utils as cfu
 import Mesh
 import calfem.vis as cfv
 import FE
+import Opt
+import Filter
+import matplotlib.pyplot as plt
 
-
-def mainer(g,el_type,force,bmarker):
-       
+def _Main(g,el_type,force,bmarker):
+     
+    """MESHING"""
     _mesh = Mesh.Mesh(g,0.05)
     
     if el_type == 2:
@@ -20,6 +23,10 @@ def mainer(g,el_type,force,bmarker):
     else:
         print("Wrong el_type!")
     
+    cfv.drawMesh(coords, edof, 2, el_type)
+    plt.pause(2)
+    
+    """ Denote forces and boundary conditions """
     nDofs = np.max(edof)
     f = np.zeros([nDofs,1])
     
@@ -31,10 +38,22 @@ def mainer(g,el_type,force,bmarker):
     
     cfu.applyforce(bdofs, f, force[1], force[0], force[2])
     
+    
+    """ Optimisation """
+    try:
+        x = Opt._Opt()
+    except:
+        print("Optimisation is not yet implemented")
     SIMP_penal = 3
     nElem=np.size(edof,0)
-    x =np.zeros([nElem,1])
-    U =FE._FE(x,SIMP_penal,edof,coords,bdofs,f)
-        
-    cfv.drawMesh(coords, edof, 2, el_type)
-    #cfv.figure()
+    x =np.zeros([nElem,1])+0.5
+    try: 
+        U =FE._FE(x,SIMP_penal,edof,coords,bdofs,f)
+    except:
+        print("FE is not done yet!")
+    
+    cfv.draw_element_values(x, coords, edof, 2, el_type, 
+                      draw_elements=True, draw_undisplaced_mesh=True, 
+                      title="Density", magnfac=25.0)
+
+    
