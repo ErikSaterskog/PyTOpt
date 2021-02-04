4e9e3a0908f38ea70bb58c4dcb31cf926112d9df
Daniel Pettersson <dapet@student.chalmers.se>
2021-02-04 09:54:15 +0100

    Testing an easy nonlinear hencky

---
+++ Modified_Hencky.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 09:04:30 2021
+
+@author: Daniel
+"""
+import numpy as np
+
+def Hencky(eps,mp):
+    E = mp[1]
+    nu = mp[2]
+    G = np.zeros([2,1])
+    G[0] = E/(2*(1+nu))
+    G[1] = -G[0].copy()
+    K = E/(3*(1-2*nu))
+    
+    I_v = np.array([1,1,1,0,0,0])
+    I_vT = np.array([[1],[1],[1],[0],[0],[0]])
+    I_s = np.diag([1,1,1,0.5,0.5,0.5])
+    I_sdev = (I_s - 1/3*I_v*I_vT)
+    
+    eps_dev = np.matmul(I_sdev,eps)
+    
+    
+    sigma = np.zeros([1,6])
+    D = np.zeros([6,6])
+    Eps_sum = sum(eps_dev*eps_dev)
+    
+    for i in range(0,2):
+        sigma = sigma + 2*G[i]*eps_dev*(1e4*Eps_sum)**i
+        D = D + 2*G[i]*I_sdev*(1e4*Eps_sum)**i
+    sigma = sigma + K*np.matmul(I_v*I_vT,eps)
+    D  = D + K*I_v*I_vT
+    
+    sigma.shape = (6,)
+    return sigma, D
---
+++ Test_Hencky.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 09:29:40 2021
+
+@author: Daniel
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import Modified_Hencky as MH
+
+E = 210e9
+nu= 0.3
+
+mp = [E,nu,0]
+
+
+eps11 = np.linspace(0,1e-2,100)
+
+eps = np.zeros([6,100])
+sigma = eps.copy()
+eps[1,:] = eps11
+for i in range(1,100):
+    sigma[:,i],D = MH.Hencky(eps[:,i],mp);
+
+plt.figure()
+plt.plot(eps[1,:],sigma[1,:])
+
+plt.plot([eps[1,1],eps[1,-1]],[sigma[1,1],sigma[1,-1]])
