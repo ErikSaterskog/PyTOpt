411845680d9f391c17f3f45ee5d1fc7d321bc2d7
Erik Säterskog <eric.saterskog@gmail.com>
2021-02-10 09:50:51 +0100

    ...

--- Debugger.py
+++ Debugger.py
@@ -9,7 +9,7 @@ import numpy as np
 
 def num_Sens_Anal(x,SIMP_penal,edof,coords,bc,f,ep,mp,numElem):
     
-    eps=1e-8
+    eps=1e-7
     dc = x.copy() 
     
     for elem in range(0,numElem):
@@ -17,14 +17,14 @@ def num_Sens_Anal(x,SIMP_penal,edof,coords,bc,f,ep,mp,numElem):
         x[elem]=x[elem]-eps
         U1 = FE._FE(x,SIMP_penal,edof,coords,bc,f,ep,mp)
     
-        x[elem]=x[elem]+eps
+        x[elem]=x[elem]+(eps*2)
         U2 = FE._FE(x,SIMP_penal,edof,coords,bc,f,ep,mp)
     
 
         G01=np.matmul(np.transpose(f),U1)
         G02=np.matmul(np.transpose(f),U2)
         
-        dc[elem]=(G02-G01)/eps
+        dc[elem]=(G02-G01)/(eps*2)
     
     return dc
 
