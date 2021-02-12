b5a775d64308a6c3509a76fde7f6c3cf51729650
Erik Säterskog <eric.saterskog@gmail.com>
2021-02-12 10:44:31 +0100

    added nonlinear

--- Debugger.py
+++ Debugger.py
@@ -9,7 +9,7 @@ import numpy as np
 
 def num_Sens_Anal(x,SIMP_penal,edof,coords,bc,f,ep,mp,numElem):
     
-    eps=1e-7
+    eps=1e-6
     dc = x.copy() 
     
     for elem in range(0,numElem):
@@ -19,6 +19,27 @@ def num_Sens_Anal(x,SIMP_penal,edof,coords,bc,f,ep,mp,numElem):
     
         x[elem]=x[elem]+(eps*2)
         U2 = FE._FE(x,SIMP_penal,edof,coords,bc,f,ep,mp)
+    
+
+        G01=np.matmul(np.transpose(f),U1)
+        G02=np.matmul(np.transpose(f),U2)
+        
+        dc[elem]=(G02-G01)/(eps*2)
+    
+    return dc
+
+def num_Sens_Anal_NL(x,SIMP_penal,edof,coords,bc,f,ep,mp,numElem):
+    
+    eps=1e-6
+    dc = x.copy() 
+    
+    for elem in range(0,numElem):
+        
+        x[elem]=x[elem]-eps
+        U1 = FE._FE_NL(x,SIMP_penal,edof,coords,bc,f,ep,mp)
+    
+        x[elem]=x[elem]+(eps*2)
+        U2 = FE._FE_NL(x,SIMP_penal,edof,coords,bc,f,ep,mp)
     
 
         G01=np.matmul(np.transpose(f),U1)
