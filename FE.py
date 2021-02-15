fe8e1847296505a65c69ad17cd654848cc349958
Erik Säterskog <eric.saterskog@gmail.com>
2021-02-15 10:27:26 +0100

    rejäla förkortningar av programmet

--- FE.py
+++ FE.py
@@ -8,7 +8,7 @@ import Plani4s
 
 
 
-def _FE(x,SIMP_penal,eDof,coords,fixDofs,F,ep,mp):
+def FE(x,SIMP_penal,eDof,coords,fixDofs,F,ep,mp):
 
     #Settings
     E=mp[0]
@@ -16,40 +16,9 @@ def _FE(x,SIMP_penal,eDof,coords,fixDofs,F,ep,mp):
     ptype=ep[0]
     Timers=True     #Print Timers 
     
-    #Check sizes
-    nDof=np.max(eDof)
-    nElem=np.size(eDof,0)
-    nx=coords[:,0]
-    ny=coords[:,1]
     
-    #Initialize Vecotors and Matrices
-    K = np.zeros([nDof,nDof])
-    #F = np.zeros([nDof,1])
-    U = np.zeros([nDof,1])
+    nDof,nElem,K,U,Tri,elemX,elemY=init(eDof,coords)
     
-    
-    #Check element type
-    if len(eDof[0,:])==6:   #Triangular Element
-        Tri=True
-        elemX=np.zeros([nElem,3])
-        elemY=np.zeros([nElem,3])
-    elif len(eDof[0,:])==8:
-        Tri=False           #Use Quad Instead
-        elemX=np.zeros([nElem,4])
-        elemY=np.zeros([nElem,4])
-    else:
-        raise Exception('Unrecognized Element Shape, Check eDof Matrix')
-
-
-    #Find The coordinates for each element's nodes
-    for elem in range(0,nElem):
-        
-        nNode=np.ceil(np.multiply(eDof[elem,:],0.5))-1
-        nNode=nNode.astype(int)
-        
-        elemX[elem,:]=nx[nNode[0:8:2]]
-        elemY[elem,:]=ny[nNode[0:8:2]]
-
     
     #Start timer
     tic1 = time.perf_counter()
@@ -109,8 +78,58 @@ def _FE_NL(x,SIMP_penal,eDof,coords,fixDofs,F,ep,mp):
     TOL=1e-6
     Timers=True     #Print Timers 
     
+    nDof,nElem,K,U,Tri,elemX,elemY=init(eDof,coords)
     
-    #Check sizes
+    D=cfc.hooke(ptype, E, v)
+    
+    allDofs = range(0,nDof)       
+    freeDofs = np.setdiff1d(allDofs, fixDofs)
+
+        
+    for elem in range(0,nElem):    #Gissning med linjärt fall
+        edofIndex=np.ix_(eDof[elem,:]-1,eDof[elem,:]-1) 
+        if Tri:
+            Ke=cfc.plante(elemX[elem,:],elemY[elem,:],ep[0:2],D)
+        else:
+            Ke=cfc.plani4e(elemX[elem,:],elemY[elem,:],ep,D)[0]
+        K[edofIndex] = K[edofIndex] + x[elem][0]**SIMP_penal*Ke
+
+    
+    U[np.ix_(freeDofs)] = spsolve(K[np.ix_(freeDofs,freeDofs)],F[np.ix_(freeDofs)]).reshape(len(freeDofs),1)
+          
+    while err>TOL:
+        K = np.zeros(np.shape(K))
+        ed=cfc.extractEldisp(eDof,U) 
+
+        for elem in range(0,nElem):
+            if Tri:
+                edofIndex=np.ix_(eDof[elem,:]-1,eDof[elem,:]-1)
+                eps = np.zeros([6,])
+                eps_2D=cfc.plants(elemX[elem,:],elemY[elem,:],ep,D,ed[elem,:])[1] 
+                eps[0:4] = eps_2D
+                D_new = mh._mod_hook(eps,mp)[1]
+                Ke=cfc.plante(elemX[elem,:],elemY[elem,:],ep[0:2],D_new[np.ix_([0,1,2,3],[0,1,2,3])])
+                K[edofIndex] = K[edofIndex] + x[elem][0]**SIMP_penal*Ke
+            else:
+                edofIndex=np.ix_(eDof[elem,:]-1,eDof[elem,:]-1)
+                eps=Plani4s.plani4s(elemX[elem,:],elemY[elem,:],ep,ed[elem,:]) 
+                D_new = mh._mod_hook(eps,mp)[1]
+                Ke=cfc.plani4e(elemX[elem,:],elemY[elem,:],ep,D_new[np.ix_([0,1,2,3],[0,1,2,3])])[0]
+                K[edofIndex] = K[edofIndex] + x[elem][0]**SIMP_penal*Ke
+                
+        R= np.matmul(K[np.ix_(freeDofs,freeDofs)],U[np.ix_(freeDofs)])-F[np.ix_(freeDofs)]
+        err = np.linalg.norm(R)
+        print(err)
+            
+        U[np.ix_(freeDofs)] = U[np.ix_(freeDofs)] - spsolve(K[np.ix_(freeDofs,freeDofs)],R).reshape(len(freeDofs),1)
+            
+        
+    return U
+
+
+
+def init(eDof,coords):
+    
     nDof=np.max(eDof)
     nElem=np.size(eDof,0)
     nx=coords[:,0]
@@ -118,13 +137,9 @@ def _FE_NL(x,SIMP_penal,eDof,coords,fixDofs,F,ep,mp):
     
     #Initialize Vecotors and Matrices
     K = np.zeros([nDof,nDof])
+    #F = np.zeros([nDof,1])
     U = np.zeros([nDof,1])
     
-    D=cfc.hooke(ptype, E, v)
-    
-    
-    allDofs = [i for i in range(0,nDof)]        
-    freeDofs = np.setdiff1d(allDofs, fixDofs)
     
     #Check element type
     if len(eDof[0,:])==6:   #Triangular Element
@@ -138,6 +153,7 @@ def _FE_NL(x,SIMP_penal,eDof,coords,fixDofs,F,ep,mp):
     else:
         raise Exception('Unrecognized Element Shape, Check eDof Matrix')
 
+
     #Find The coordinates for each element's nodes
     for elem in range(0,nElem):
         
@@ -146,85 +162,8 @@ def _FE_NL(x,SIMP_penal,eDof,coords,fixDofs,F,ep,mp):
         
         elemX[elem,:]=nx[nNode[0:8:2]]
         elemY[elem,:]=ny[nNode[0:8:2]]
-    
-    
-
-    #check element shape
-    if Tri==True:
-    
         
-        for elem in range(0,nElem):    #Gissning med linjärt fall
-            edofIndex=np.ix_(eDof[elem,:]-1,eDof[elem,:]-1) 
-            Ke=cfc.plante(elemX[elem,:],elemY[elem,:],ep[0:2],D)
-            K[edofIndex] = K[edofIndex] + x[elem][0]**SIMP_penal*Ke
-
-    
-        U[np.ix_(freeDofs)] = spsolve(K[np.ix_(freeDofs,freeDofs)],F[np.ix_(freeDofs)]).reshape(len(freeDofs),1)
-          
-        while err>TOL:
-            K = np.zeros(np.shape(K))
-            ed=cfc.extractEldisp(eDof,U) 
-
-            for elem in range(0,nElem):
-                edofIndex=np.ix_(eDof[elem,:]-1,eDof[elem,:]-1)
-                eps = np.zeros([6,])
-                eps_2D=cfc.plants(elemX[elem,:],elemY[elem,:],ep,D,ed[elem,:])[1] 
-                eps[0:4] = eps_2D
-                D_new = mh._mod_hook(eps,mp)[1]
-                Ke=cfc.plante(elemX[elem,:],elemY[elem,:],ep[0:2],D_new[np.ix_([0,1,2,3],[0,1,2,3])])
-                K[edofIndex] = K[edofIndex] + x[elem][0]**SIMP_penal*Ke
-                
-            R= np.matmul(K[np.ix_(freeDofs,freeDofs)],U[np.ix_(freeDofs)])-F[np.ix_(freeDofs)]
-            err = np.linalg.norm(R)
-            print(err)
-            
-            U[np.ix_(freeDofs)] = U[np.ix_(freeDofs)] - spsolve(K[np.ix_(freeDofs,freeDofs)],R).reshape(len(freeDofs),1)
-            
-    
-            
-    else:
-        for elem in range(0,nElem):    #Gissning med linjärt fall
-            edofIndex=np.ix_(eDof[elem,:]-1,eDof[elem,:]-1) 
-            Ke=cfc.plani4e(elemX[elem,:],elemY[elem,:],ep,D)[0]
-            K[edofIndex] = K[edofIndex] + x[elem][0]**SIMP_penal*Ke
-
-    
-        U[np.ix_(freeDofs)] = spsolve(K[np.ix_(freeDofs,freeDofs)],F[np.ix_(freeDofs)]).reshape(len(freeDofs),1)
-        
-        while err>TOL:
-            K = np.zeros(np.shape(K))
-            ed=cfc.extractEldisp(eDof,U) 
-
-            for elem in range(0,nElem):
-                edofIndex=np.ix_(eDof[elem,:]-1,eDof[elem,:]-1)
-                eps=Plani4s.plani4s(elemX[elem,:],elemY[elem,:],ep,ed[elem,:]) 
-                
-                D_new = mh._mod_hook(eps,mp)[1]
-                Ke=cfc.plani4e(elemX[elem,:],elemY[elem,:],ep,D_new[np.ix_([0,1,2,3],[0,1,2,3])])[0]
-                K[edofIndex] = K[edofIndex] + x[elem][0]**SIMP_penal*Ke
-                
-            R= np.matmul(K[np.ix_(freeDofs,freeDofs)],U[np.ix_(freeDofs)])-F[np.ix_(freeDofs)]
-            err = np.linalg.norm(R)
-            print(err)
-            
-            U[np.ix_(freeDofs)] = U[np.ix_(freeDofs)] - spsolve(K[np.ix_(freeDofs,freeDofs)],R).reshape(len(freeDofs),1)
-            
-        
-        
-        
-        #raise Exception ('QUADS NOT YET IMPLEMENTED')
-        
-    return U
-
-#
-#
-#
-#
-#
-#
-#
-#
-
+    return nDof,nElem,K,U,Tri,elemX,elemY
 
 
 
