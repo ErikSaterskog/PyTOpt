2d957d7924e08988e32a85794bc53f17bbe660c9
Erik Säterskog <eric.saterskog@gmail.com>
2021-02-15 10:34:37 +0100

    ...

--- Test_Examples/Cantilever_Beam.py
+++ Test_Examples/Cantilever_Beam.py
@@ -12,25 +12,30 @@ import calfem.geometry as cfg
 import calfem.vis as cfv
 import Main
 
-el_type = 2   #2-Tri,  3-Quad
 g = cfg.Geometry()
 
 g.point([0,0])                 #0
-g.point([1,0],marker=9)        #1
-g.point([1,0.5])               #2
-g.point([0,0.5])               #3
+g.point([1,0])        #1
+g.point([1,0.5],marker=9)               #2               #2
+g.point([1,1])               #2
+g.point([0,1])               #3
+g.point([0,0.6])               #3
+g.point([0,0.4])               #3
 
 
 g.spline([0, 1],marker=0)
 g.spline([1, 2],marker=1)
 g.spline([2, 3],marker=2)
-g.spline([3, 0],marker=4)
+g.spline([3, 4],marker=3)
+g.spline([4, 5],marker=4)
+g.spline([5, 6],marker=5)
+g.spline([6, 0],marker=6)
 
 
-g.surface([0, 1, 2, 3])
+g.surface([0, 1, 2, 3, 4,5,6])
 
-force = [-1e5,9,2] #First magnitude, second marker, third direction
-bmarker = 4
+force = [-1e8,9,2] #First magnitude, second marker, third direction
+bmarker = 5
 
 
 E = 210e9 # Young's modulus
@@ -38,11 +43,11 @@ nu = 0.3 #Poisson's ratio
 
 mp = [E,nu]
 
-volFrac = 0.5 # Constraint on 50% volume
-meshSize=0.1 # How fine mesh we want. 1 is only one element and 0 is infinity.
-rMin = meshSize*np.sqrt(2)*1 # How aggressive the filter should be. Smaller -> less aggressive
+volFrac = 0.2 # Constraint on 50% volume
+meshSize=0.04 # How fine mesh we want. 1 is only one element and 0 is infinity.
+rMin = meshSize*np.sqrt(2)*0.5 # How aggressive the filter should be. Smaller -> less aggressive
 changeLimit=0.005 # How small change between two optmisation we allow before stopping.
-
+el_type = 3   #2-Tri,  3-Quad
 
 
 
