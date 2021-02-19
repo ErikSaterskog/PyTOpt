fba7cc89c5de37f7ef1d83b715bbca99428a702f
Erik Säterskog <eric.saterskog@gmail.com>
2021-02-19 10:45:34 +0100

    major perfomance boost

--- Filter.py
+++ Filter.py
@@ -16,12 +16,27 @@ def Check(x,dc,H):
 
 
     #new_dc=np.transpose((np.sum(np.transpose(x)*H[:,:]*np.transpose(dc), axis=1))/(np.transpose(x[:])*np.sum(H[:,:], axis=1)))
+    
+    
+    #dc=dc.reshape(len(dc))
+    #H[:,elem].toarray().reshape(len(dc))
+    #x=x.reshape(len(dc))
+    
     #breakpoint()
-    #breakpoint()
-    #breakpoint()
-    for elem in range(nElem):
+    
+    Hsum=H.sum(axis=1).tolist()
+    
+    new_dc=H.multiply(x).multiply(dc).sum(axis=0).transpose()/(Hsum*x)
+    
 
-        new_dc[elem]=(sum(dc.reshape(len(dc))*H[:,elem].toarray().reshape(len(dc))*x.reshape(len(dc))))/(np.sum(H[elem,:]*x[elem][0]))
+    #breakpoint()
+
+    #for elem in range(nElem):
+    #    breakpoint()
+    #    new_dc[elem]=(sum(dc*H[elem,:].toarray()[0]*x))/(np.sum(H[elem,:]*x[elem]))
+        
+        
+        
         #new_dc[elem]=(x*np.sum(H, axis=1).tolist()/(x[elem]*np.sum(H, axis=1).tolist()))*dc[elem]
         
     #    new_dc[elem]=dc[elem]*((H[elem,:]*x[:])/((x[elem])*np.sum(H[elem,:])))
