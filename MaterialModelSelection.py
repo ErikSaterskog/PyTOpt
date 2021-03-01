90d1141160469bb500d923eacfcacdd832e68c61
Erik Säterskog <eric.saterskog@gmail.com>
2021-03-01 10:32:54 +0100

    ...

--- MaterialModelSelection.py
+++ MaterialModelSelection.py
@@ -1,11 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 24 12:10:37 2021
 
-@author: Daniel
-"""
+
 import elem3n
 import elem4n
+import calfem.core as cfc
 
 def Tri(ue,ex,ey,ep,mp,eq=None):
     return elem3n.elem3n(ue, ex, ey, ep, mp, eq=None)
@@ -13,3 +10,8 @@ def Tri(ue,ex,ey,ep,mp,eq=None):
 def Quad(ue,ex,ey,ep,mp,eq=None):
     return elem4n.elem4n(ue, ex, ey, ep, mp, eq=None)
 
+def LinTri(ex,ey,ep,D,eq=None):
+    return cfc.plante(ex, ey, ep, D, eq=None)
+
+def LinQuad(ex, ey, ep, D, eq=None):
+    return cfc.plani4e(ex, ey, ep, D, eq=None)
