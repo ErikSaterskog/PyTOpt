6ed323c502eb640e9ffd7ba74f2df074697f2f95
Erik Säterskog <eric.saterskog@gmail.com>
2021-03-08 13:46:19 +0100

    ...

--- ErikTestMaterial.py
+++ ErikTestMaterial.py
@@ -10,8 +10,8 @@ def mat(eps,mp):
     E_com       = E_ten*3
     nu_ten      = 0.3
     nu_com      = 0.3
-    low_lim=0.0005
-    high_lim=0.0025
+    low_lim=0.0001
+    #high_lim=0.0025
     
     eps_h = sum(eps[:2])/3
     
@@ -20,21 +20,9 @@ def mat(eps,mp):
     D_ten=Dfun(E_ten,nu_ten)
     D_com=Dfun(E_com,nu_com) 
 
-    if eps_h < low_lim:
-        #comp
-        print('comp')
-        ten_quote=0
-        
-    else:# eps_h > low_lim and eps_h < high_lim:
-        #ten
-        print('ten')
-        #ten_quote=np.min([((eps_h-low_lim)/(high_lim-low_lim)),1])
-        x=4500*(eps_h-low_lim)
-        ten_quote=1-np.exp(-x+4)/(1+np.exp(-x+4))
-        print(ten_quote)
-        #breakpoint()
+    x=2500*(eps_h-low_lim)
+    ten_quote=1-np.exp(-x+4)/(1+np.exp(-x+4))
 
-        
     comp_quote=1-ten_quote
     D=D_com*comp_quote + D_ten*ten_quote 
     sigma[:4] = np.matmul(D,eps[:4])
