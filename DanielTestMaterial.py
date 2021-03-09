cbee0bf9ab3d763641f930c5f72d1a9fb17b8ce1
Daniel Pettersson <dapet@student.chalmers.se>
2021-03-09 13:29:40 +0100

    Numeric D

--- DanielTestMaterial.py
+++ DanielTestMaterial.py
@@ -15,7 +15,7 @@ def mat(eps,mp):
     nu      = 0.3
     low_lim=0.0005
     high_lim=0.0025
-    sig_y = 220e6
+    sig_y = 220e5
     G = E/(2*(1+nu))
     K = E/(3*(1-2*nu))
     
@@ -32,13 +32,16 @@ def mat(eps,mp):
         
     if eps_h >= sig_y/(3*G): 
         Gstar = sig_y/(3*eps_h)
-        sigma = 2*Gstar*eps_dev + K*np.matmul(I_v*I_vT,eps)
         Estar = 2*Gstar*(1+nu)
-        D = Dfun(Estar,nu)
-        #D = 2*Gstar*I_sdev - 4*sig_y/(9*eps_h**3)*np.matmul(eps_dev.reshape(6,1),eps_dev.reshape(1,6)) + K*I_v*I_vT
+        Kstar = Estar/(3*(1-2*nu))
+        sigma = 2*Gstar*eps_dev + Kstar*np.matmul(I_v*I_vT,eps)
+        D = numD(eps,eps_dev,Gstar,Kstar,I_v,I_vT,I_sdev)
+        #D = Dfun(Estar,nu)
+        #D = 2*Gstar*I_sdev - 4*sig_y/(9*eps_h**3)*np.matmul(eps_dev.reshape(6,1),eps_dev.reshape(1,6)) + Kstar*I_v*I_vT
     else:
         sigma = 2*G*eps_dev + K*np.matmul(I_v*I_vT,eps)
         D = Dfun(E,nu)
+        #D1 = numD(eps,eps_dev,G,K,I_v,I_vT,I_sdev)
 
 
     
@@ -52,6 +55,18 @@ def mat(eps,mp):
 def Dfun(E,nu):
     return E/((1+nu)*(1-2*nu))*np.array([[1-nu,nu,nu,0],[nu,1-nu,nu,0],[nu,nu,1-nu,0],[0,0,0,(1-2*nu)/2]])
 
-
+def numD(eps,eps_dev,Gstar,Kstar,I_v,I_vT,I_sdev):
+    delta = 1e-7
+    sig1 = 2*Gstar*eps_dev + Kstar*np.matmul(I_v*I_vT,eps)
+    D = np.zeros([6,6])
+    for i in range(0,6):
+        eps2 = eps.copy()
+        eps2[i] = eps[i] + delta
+        eps_dev2 =  np.matmul(I_sdev,eps2)
+        sig2 = 2*Gstar*eps_dev2 + Kstar*np.matmul(I_v*I_vT,eps2)
+        
+        D[i,:] = -(sig1-sig2)/delta
+    return D
+    
 
 
