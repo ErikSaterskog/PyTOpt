f2e89a8dea871125c7a654dba3cc3e188e41e143
Daniel Pettersson <dapet@student.chalmers.se>
2021-03-18 10:30:58 +0100

    ...

--- TestMaterial.py
+++ TestMaterial.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def head(eps,mp):
     
-    sig, D = mat(eps,mp)
+    sig,D = mat(eps,mp)
     
     #D = numD(eps,sig,mp)
     
@@ -16,29 +16,28 @@ def mat(eps,mp):
     nu1      = mp[1]
     eps_y    = mp[2]
     
-    E2 = E1*0.4
+    E2 = E1*0.5
     nu2 = nu1
     
-    G1 = E1/(2*(1+nu1))
-    K1 = E1/(3*(1-2*nu1))
-    G2 = E2/(2*(1+nu2))
-    K2 = E2/(3*(1-2*nu2))
+    G = E1/(2*(1+nu1))
+    K1 = E1*G/(3*(3*G-E1))
+    K2 = E2*G/(3*(3*G-E2))
+    
+    
+    
     
     eps_h = sum(eps[:2])/3
     
-    I_v = np.array([1,1,1,0,0,0])
+    I_v = np.array([[1,1,1,0,0,0]])
     I_vT = np.array([[1],[1],[1],[0],[0],[0]])
     I_s = np.diag([1,1,1,0.5,0.5,0.5])
     I_sdev = (I_s - 1/3*I_v*I_vT)
     
     dkdeps = np.zeros([1,6])   
-    if eps_h >= eps_y:
+    if eps_h > eps_y:
         k = 1-eps_y/eps_h
-        for i in [0,1,3]:
-            if abs(eps[i])>1e-20: 
-                dkdeps[0,i] = eps_y/(eps_h*3*eps[i])
-            else:
-                dkdeps[0,i] = 0
+        dkdeps = I_v*3*eps_y/eps_h**2
+        
     else:
         k = 0
         
@@ -49,14 +48,17 @@ def mat(eps,mp):
     eps_dev2 = np.matmul(I_sdev,eps2)
         
         
-    sigma = 2*G1*eps_dev1 + K1*np.matmul(I_v*I_vT,eps1) + 2*G2*eps_dev2 + K2*np.matmul(I_v*I_vT,eps2)
+    sigma = 2*G*eps_dev1 + K1*np.matmul(I_v*I_vT,eps1) + 2*G*eps_dev2 + K2*np.matmul(I_v*I_vT,eps2)
 
     #D = (2*G1*I_sdev + K1*I_v*I_vT)*(1-k) + (2*G1*I_sdev + K1*I_v*I_vT)*k
     
-    D = (2*G1*I_sdev + K1*I_v*I_vT)*(1-k+eps.reshape(6,1)*dkdeps) + (2*G1*I_sdev + K1*I_v*I_vT)*(k-eps.reshape(6,1)*dkdeps)
+    D = (2*G*I_sdev + K1*I_v*I_vT)*(1-k)
+    -(2*G*I_sdev+ K1*I_v*I_vT)*np.matmul(dkdeps,eps.reshape(6,1))
+    +(2*G*I_sdev + K2*I_v*I_vT)*k
+    +(2*G*I_sdev + K2*I_v*I_vT)*np.matmul(dkdeps,eps.reshape(6,1))
     
     
-    return sigma, D
+    return sigma,D
 
 
 
