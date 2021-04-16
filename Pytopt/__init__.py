d0a44c06a76539bd0af76f35081f28e67983d106
Biggorili <77610382+Biggorili@users.noreply.github.com>
2021-04-16 09:44:08 +0200

    Update __init__.py

--- Pytopt/__init__.py
+++ Pytopt/__init__.py
@@ -1,18 +1,1 @@
-from Pytopt import Debugger
-from Pytopt import Element_Quad_Routine 
-from Pytopt import Element_Tri_Routine
-from Pytopt import Element_Routine_Selection as ERS
-from Pytopt import FE
-from Pytopt import Filter
-from Pytopt import Material_Bilinear
-from Pytopt import Material_Elastic
-from Pytopt import Material_ModifiedHooke
-from Pytopt import Material_Routine_Selection as mrs
-from Pytopt import Mesh
-from Pytopt import MMA_fun
-from Pytopt import Object_Func_Displacement
-from Pytopt import Object_Func_Energy
-from Pytopt import Object_Func_Selection as ofs
-from Pytopt import Optimisation as Opt
-from Pytopt import PyTOpt
-from Pytopt import Test_NLMaterialmodel
+
