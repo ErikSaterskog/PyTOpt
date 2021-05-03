ab29a72117a9013cf7bc1ca49039d027597d8e28
Daniel Pettersson <dapet@student.chalmers.se>
2021-05-03 11:32:11 +0200

    Added the linear part

--- Pytopt/Object_Func_Displacement.py
+++ Pytopt/Object_Func_Displacement.py
@@ -2,6 +2,7 @@
 
 
 import numpy as np
+import calfem.core as cfc
 from scipy.sparse.linalg import spsolve
 
 
@@ -15,24 +16,47 @@ def Displacement(nelem, ep, el_type, elemx, elemy, D, eq, U, edof, fext_tilde, f
     lambdaF = np.zeros(U.shape)
     lambdaF[index1D] = -2*spsolve(K[index2D],U[index1D]).reshape(len(freedofs),1)
 
-    for elem in range(nelem):  
-        if eq is not None:
-            eqe=[eq,eq,eq]
-            eqe=np.array(eqe).reshape(1,6)
-        else:
-            eqe=np.zeros([1,6])
+    for elem in range(nelem): 
+        if ep[3]:
+            if el_type==2:
+                Ke=cfc.plante(elemx[elem,:],elemy[elem,:],ep[0:2],D) #Element Stiffness Matrix for Triangular Element
+                if eq is not None:
+                    eqe=[eq,eq,eq]
+                    eqe=np.array(eqe).reshape(1,6)
+                else:
+                        eqe=np.zeros([1,6])
+            else:
+                Ke=cfc.plani4e(elemx[elem,:],elemy[elem,:],ep,D)[0]  #Element Stiffness Matrix for Quad Element
+                if eq is not None:
+                    eqe=[eq,eq,eq,eq]
+                    eqe=np.array(eqe).reshape(1,8)
+                else:
+                    eqe=np.zeros([1,8])
 
-        lambdaFe = lambdaF[np.ix_(edof[elem,:]-1)]
-        
-
-        dG0[elem] = np.matmul(lambdaFe.T,dR[elem,:].reshape(np.size(edof,1),1))
-                    
-        if dG0[elem] >0:
-            print(str(elem) + ':' +str(dG0[elem]))
-            dG0[elem] = 0
+            Ue = U[np.ix_(edof[elem,:]-1)]
+            lambdaFe = lambdaF[np.ix_(edof[elem,:]-1)]
+            dG0[elem] =  SIMP_penal*x[elem][0]**(SIMP_penal-1)*np.matmul(lambdaFe.T, np.matmul(Ke,Ue))
                 
-        G0=np.matmul(fextGlobal.T,U)
-
+                    
+        else:
+                        
+            if eq is not None:
+                eqe=[eq,eq,eq]
+                eqe=np.array(eqe).reshape(1,6)
+            else:
+                eqe=np.zeros([1,6])
+    
+            lambdaFe = lambdaF[np.ix_(edof[elem,:]-1)]
+            
+    
+            dG0[elem] = np.matmul(lambdaFe.T,dR[elem,:].reshape(np.size(edof,1),1))
+                        
+            if dG0[elem] >0:
+                print(str(elem) + ':' +str(dG0[elem]))
+                dG0[elem] = 0
+                    
+            
+    G0=np.matmul(U.T,U)
     return G0, dG0
 
 
